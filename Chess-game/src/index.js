9f9ac4cc2263ee92619c451270301e486e9417ea
H.M.E <1316126217@qq.com>
2021-07-29 11:02:02 +0800

    Chess-game

---
+++ Chess-game/src/index.js
@@ -0,0 +1,25 @@
+var Player;
+(function (Player) {
+    Player["X"] = "x";
+    Player["O"] = "o";
+})(Player || (Player = {}));
+var cells = document.querySelectorAll('.cell');
+var gameBord = document.querySelector('#bord');
+// 当前玩家
+// let currentPlayer:Player = Player.X
+var currentPlayer = Player.X;
+cells.forEach(function (item) {
+    // console.log(item)
+    var cell = item;
+    cell.addEventListener('click', clickcell, { once: true });
+});
+// 棋盘中单元格的click时间处理程序
+function clickcell(event) {
+    var target = event.target;
+    target.classList.add(currentPlayer);
+    // 根据当前玩家，得到另一个玩家
+    currentPlayer = currentPlayer === Player.X ? Player.O : Player.X;
+    // 处理下一步提示
+    gameBord.classList.remove(Player.X, Player.O);
+    gameBord.classList.add(currentPlayer);
+}
---
+++ Chess-game/src/index.ts
@@ -0,0 +1,31 @@
+enum Player {
+  X = 'x',
+  O = 'o'
+}
+
+let  cells  = document.querySelectorAll('.cell')
+
+let  gameBord  = document.querySelector('#bord')
+// 当前玩家
+// let currentPlayer:Player = Player.X
+let currentPlayer = Player.X
+cells.forEach(function(item){
+  // console.log(item)
+  let  cell  =item  as  HTMLDivElement
+  cell.addEventListener('click',clickcell,{once:true})
+})
+
+// 棋盘中单元格的click时间处理程序
+
+function  clickcell(event:MouseEvent){
+  let  target = event.target  as  HTMLDivElement
+  target.classList.add(currentPlayer)
+  
+  // 根据当前玩家，得到另一个玩家
+currentPlayer = currentPlayer === Player.X ? Player.O : Player.X
+// 处理下一步提示
+gameBord.classList.remove(Player.X,Player.O)
+gameBord.classList.add(currentPlayer)
+}
+
+
