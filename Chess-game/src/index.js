388028a4f09efd0a0eeb2465815db639db8aecf6
H.M.E <1316126217@qq.com>
2021-08-04 10:13:57 +0800

    over

--- Chess-game/src/index.js
+++ Chess-game/src/index.js
@@ -10,23 +10,71 @@ var winsArr = [
     [0, 4, 8], [2, 4, 6] //斜
 ];
 var cells = document.querySelectorAll('.cell');
+// 游戏面板
 var gameBord = document.querySelector('#bord');
 // 当前玩家
+// 获取信息面板
+var message = document.querySelector('#message');
+// 获胜者
+var winner = document.querySelector('#winner');
 // let currentPlayer:Player = Player.X
-var currentPlayer = Player.X;
-cells.forEach(function (item) {
-    // console.log(item)
-    var cell = item;
-    cell.addEventListener('click', clickcell, { once: true });
-});
+var currentPlayer;
+// 重新开始按钮
+var restart = document.querySelector('#restart');
+// console.log(restart)
+//记录已下棋的次数
+var steps;
+// 调用函数来初始化游戏数据，开始游戏
+startGame();
+// 重新游戏
+restart.addEventListener('click', startGame);
+function startGame() {
+    // console.log('click')
+    // 重置获胜信息，
+    message.style.display = 'none';
+    // 重置下棋次数
+    steps = 0;
+    // 重置默认玩家为x
+    currentPlayer = Player.X;
+    // 重置下棋提示为x
+    gameBord.classList.remove(Player.X, Player.O);
+    gameBord.classList.add(Player.X);
+    // 给单元格绑定点击事件
+    // 清空棋盘
+    cells.forEach(function (item) {
+        var cell = item;
+        // 清空棋盘
+        cell.classList.remove(Player.X, Player.O);
+        // 移除单元格点击事件，重新给单元格绑定点击事件
+        cell.removeEventListener('click', clickcell);
+        cell.addEventListener('click', clickcell, { once: true });
+    });
+}
+// 给单元格绑定点击事件 
+// cells.forEach(function (item) {
+//   // console.log(item)
+//   let cell = item as HTMLDivElement
+//   cell.addEventListener('click', clickcell, { once: true })
+// })
 // 棋盘中单元格的click时间处理程序
 function clickcell(event) {
     var target = event.target;
     target.classList.add(currentPlayer);
+    steps++;
     // 调用判嬴函数判断是否获胜
     var isWin = checkWin(currentPlayer);
     if (isWin) {
-        console.log('当前玩家获胜了', currentPlayer);
+        message.style.display = 'block';
+        winner.innerText = currentPlayer + '赢了';
+        // console.log('当前玩家获胜了', currentPlayer)
+        // 因为游戏已经结束，所以，此处直接return
+        return;
+    }
+    if (steps === 9) {
+        // 
+        message.style.display = 'block';
+        winner.innerText = '平局';
+        return;
     }
     // 根据当前玩家，得到另一个玩家
     currentPlayer = currentPlayer === Player.X ? Player.O : Player.X;
--- Chess-game/src/index.ts
+++ Chess-game/src/index.ts
@@ -11,29 +11,85 @@ let winsArr = [
 ]
 
 let cells = document.querySelectorAll('.cell')
-
+// 游戏面板
 let gameBord = document.querySelector('#bord')
 // 当前玩家
+// 获取信息面板
+let message = document.querySelector('#message') as HTMLDivElement
+// 获胜者
+let winner = document.querySelector('#winner') as HTMLParagraphElement
 // let currentPlayer:Player = Player.X
-let currentPlayer = Player.X
-cells.forEach(function (item) {
-  // console.log(item)
-  let cell = item as HTMLDivElement
-  cell.addEventListener('click', clickcell, { once: true })
-})
+let currentPlayer:Player
+// 重新开始按钮
+let restart = document.querySelector('#restart') as HTMLButtonElement
+// console.log(restart)
+//记录已下棋的次数
+let steps:number
+
+// 调用函数来初始化游戏数据，开始游戏
+startGame()
+
+
+// 重新游戏
+restart.addEventListener('click', startGame)
+
+
+
+function startGame() {
+  // console.log('click')
+  // 重置获胜信息，
+  message.style.display = 'none'
+  // 重置下棋次数
+  steps = 0
+  // 重置默认玩家为x
+  currentPlayer = Player.X
+  // 重置下棋提示为x
+  gameBord.classList.remove(Player.X, Player.O)
+  gameBord.classList.add(Player.X)
+  // 给单元格绑定点击事件
+  // 清空棋盘
+  cells.forEach(function (item) {
+    let cell = item as HTMLDivElement
+
+    // 清空棋盘
+    cell.classList.remove(Player.X, Player.O)
+    // 移除单元格点击事件，重新给单元格绑定点击事件
+    cell.removeEventListener('click',clickcell)
+    cell.addEventListener('click', clickcell, { once: true })
+  })
+}
+
+
+// 给单元格绑定点击事件 
+// cells.forEach(function (item) {
+//   // console.log(item)
+//   let cell = item as HTMLDivElement
+//   cell.addEventListener('click', clickcell, { once: true })
+// })
 
 // 棋盘中单元格的click时间处理程序
-
 function clickcell(event: MouseEvent) {
   let target = event.target as HTMLDivElement
   target.classList.add(currentPlayer)
-
+  steps++
   // 调用判嬴函数判断是否获胜
   let isWin = checkWin(currentPlayer)
   if (isWin) {
-    console.log('当前玩家获胜了', currentPlayer)
+    message.style.display = 'block'
+    winner.innerText = currentPlayer + '赢了'
+    // console.log('当前玩家获胜了', currentPlayer)
+
+    // 因为游戏已经结束，所以，此处直接return
+    return
 
   }
+  if (steps === 9) {
+    // 
+    message.style.display = 'block'
+    winner.innerText = '平局'
+    return
+  }
+  
 
   // 根据当前玩家，得到另一个玩家
   currentPlayer = currentPlayer === Player.X ? Player.O : Player.X
@@ -41,6 +97,8 @@ function clickcell(event: MouseEvent) {
   gameBord.classList.remove(Player.X, Player.O)
   gameBord.classList.add(currentPlayer)
 }
+
+
 
 // 封装判嬴函数
 function checkWin(player: Player) {
@@ -75,6 +133,8 @@ function checkWin(player: Player) {
   })
 
 }
+
+
 // 封装  hasClass函数 ：判断DOM元素 是否包含某个类名
 function hasClass(el: Element, name: string) {
   return el.classList.contains(name)
